1a96878781846cad9fe86d56121912ce399218fe
synacker <milovidovmikhail@gmail.com>
2020-05-17 20:32:01 +0300

    Added gitignore. Added generator for ex3 via faker

---
+++ GeekBrains_Python_Base_Work_With_Files_Practice/generators.py
@@ -0,0 +1,12 @@
+from random import randint
+from faker import Faker
+
+
+def ex3_gen(filename, size=10, salary_base=20000):
+    Faker.seed(0)
+    fake = Faker()
+    with open(filename, 'w') as f:
+        for i in range(0, size):
+            print(f'{fake.last_name()} {randint(salary_base / 2, salary_base * 1.5)}', file=f)
+
+
