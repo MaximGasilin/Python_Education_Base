92aa47d3898d6a0cbaddb883a525298f6524ffa0
synacker <milovidovmikhail@gmail.com>
2020-05-17 21:53:36 +0300

    Ex6 generator

--- GeekBrains_Python_Base_Work_With_Files_Practice/generators.py
+++ GeekBrains_Python_Base_Work_With_Files_Practice/generators.py
@@ -22,4 +22,20 @@ def ex3_gen(filename, size=10, salary_base=20000):
 def ex4_gen(filename, count=20):
     with open(filename, 'w') as f:
         for digit in range(0, count):
-            print(f'{num2words(digit)} - {digit}', file=f)
+            print(f'{num2words(digit)}-{digit}', file=f)
+
+
+LIST_OF_SUBJECTS = ('Математика', 'Русский язык', 'Литература', 'Физика', 'Химия', 'Биология', 'Иностранный язык',
+                    'Труд', 'Физкультура', 'География', 'Черчение', 'Астрономия', 'ОБЖ', 'Информатика')
+
+
+def ex6_gen(filename):
+    with open(filename, 'w', encoding='utf-8') as file_obj:
+        for el in LIST_OF_SUBJECTS:
+            plan_le = randint(0, 101)
+            plan_pr = randint(0, 21)
+            plan_lb = randint(0, 11)
+
+            print(f'{el}: {f"{plan_le} (л)" if plan_le > 0 else ""}'
+                  f'{f" - {plan_pr} (пр)" if plan_pr > 0 else " - "}'
+                  f'{f" - {plan_lb} (лаб)" if plan_lb > 0 else " - "}', file=file_obj)
