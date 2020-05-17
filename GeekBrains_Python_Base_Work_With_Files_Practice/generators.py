606a9a6baeb5437f89e0557b3bc6c9d02ca35a65
synacker <milovidovmikhail@gmail.com>
2020-05-17 20:43:50 +0300

    Added ex2 generator

--- GeekBrains_Python_Base_Work_With_Files_Practice/generators.py
+++ GeekBrains_Python_Base_Work_With_Files_Practice/generators.py
@@ -2,6 +2,14 @@ from random import randint
 from faker import Faker
 
 
+def ex2_gen(filename, lines = 10):
+    Faker.seed(0)
+    fake = Faker()
+    with open(filename, 'w') as f:
+        for i in range(0, lines):
+            print(f'{fake.text()}', file=f)
+
+
 def ex3_gen(filename, size=10, salary_base=20000):
     Faker.seed(0)
     fake = Faker()
